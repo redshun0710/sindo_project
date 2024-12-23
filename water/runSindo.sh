e9f6cf9453bf9db5b2600b951016bc08ac718b4f
redshun0710 <konnoshun1985@kyotounivcoop.onmicrosoft.com>
2024-12-24 00:32:20 +0900

    water

---
+++ water/runSindo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+. /path/to/sindo/sindovars.sh
+export POTDIR=./
+
+sindo < vscf.inp   > vscf.out   2>&1
+sindo < vmp2.inp   > vmp2.out   2>&1
+sindo < vqdpt2.inp > vqdpt2.out 2>&1
+sindo < vci.inp    > vci.out    2>&1
+
